c3e055257404c5eca755d9f64c92aafcf42f26fa
Jaydeep <jaydeeppatil283@gmail.com>
2025-07-21 11:40:17 +0530

    Added user routes

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -5,4 +5,4 @@ api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
 api_router.include_router(transcriptions.router, prefix="/transcriptions", tags=["transcriptions"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
